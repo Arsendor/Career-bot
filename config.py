55e6bdda3a2ecc522082e84c1c0bbc7aba7fa19b
RefiqQanizade2012 <134130684+RefiqQanizade2012@users.noreply.github.com>
2025-11-16 18:28:23 +0400

    Clear TOKEN and API_KEY in config.py
    Updated TOKEN and API_KEY variables to be empty strings.

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
-TOKEN = "8477987059:AAFXLEre7CQxAEDPWbXF08O54Dk4yUQJlZc"
+TOKEN = "" # <-- вставь сюда свой телеграм бот токен
 DATABASE = "career_bot.db"
-API_KEY = "7659f1acbff34f71b8d75f51765bb8b9"  # <-- вставь сюда свой ключ
+
+API_KEY = ""  # <-- вставь сюда свой ключ
